6841b053488ef34f35b5274e581887e07c3988fc
dillandatanerd <dillanpatelnz@gmail.com>
2025-09-10 15:28:27 +0200

    feat: User schema

--- models/User.model.js
+++ models/User.model.js
@@ -1,25 +1,41 @@
-// ❗This is an example of a User Model. 
+// ❗This is an example of a User Model.
 // TODO: Please make sure you edit the User model to whatever makes sense in your project.
 
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
+    firstName: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      required: true,
+    },
     email: {
       type: String,
-      required: [true, 'Email is required.'],
+      required: [true, "Email is required."],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
     password: {
       type: String,
-      required: [true, 'Password is required.']
-    }
+      required: [true, "Password is required."],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "warehouse-manager", "picker", "guest"],
+    },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
   }
 );
 
